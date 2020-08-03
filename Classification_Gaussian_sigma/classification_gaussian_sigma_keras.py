04424892096d200a92de549090bd5750d66a16f1
jgshin354 <jgshin354@gmail.com>
2020-08-03 10:19:46 +0900

    Classification_Gaussian_sigma changes
    Since 'Classification_Gaussian_sigma.py' is based on the keras, it is renamed as 'Classification_Gaussian_sigma_keras.py'.

--- Classification_Gaussian_sigma/classification_gaussian_sigma_keras.py
+++ Classification_Gaussian_sigma/classification_gaussian_sigma_keras.py
@@ -10,14 +10,13 @@ from keras.layers import MaxPooling2D
 from keras.layers import Conv2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras.callbacks import ReduceLROnPlateau, EarlyStopping
-from keras import utils, models, layers, optimizers
+
 import numpy as np
 import os
 from PIL import Image
 import os, glob
 import matplotlib.pyplot as plt
 import cv2 as cv
-from tensorflow.keras.utils import multi_gpu_model                              ###in case of using multi GPU
 
 flag_train_analysis = True
 flag_specific_test_image_appl_0 = False
@@ -51,23 +50,40 @@ else:
         test.append(img)
     X_train = np.array(test)
 
+#%%
+# 모델 구조 정의 
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=X_train.shape[1:], padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(32, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
 
-#%% 모델 구조 정의 / 네트워크 생성
-import efficientnet.keras as efn
-from iterstrat.ml_stratifiers import MultilabelStratifiedShuffleSplit
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(64, (3, 3), padding='same'))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
 
-base_model = efn.EfficientNetB7(weights = 'imagenet', include_top = False, pooling = 'avg', input_shape = (100, 100, 3))
-x = base_model.output
-x = Dropout(0.5)(x)
-x = Dense(1024, activation='relu')(x)
-output = Dense(nb_classes, activation = 'softmax')(x)
-model = Model(base_model.input, output)
-#model = multi_gpu_model(model, gpus = 2)                                       ###in case of using multi GPU
-model.summary()
-model.compile(optimizer=optimizers.Adam(lr = 1e-5), loss = 'categorical_crossentropy',  metrics=['accuracy'])
+# 전결합층
+model.add(Flatten())    # 벡터형태로 reshape
+model.add(Dense(128))   # 출력
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
 
+model.add(Dense(nb_classes))
+model.add(Activation('softmax'))
+# 모델 구축하기
+model.compile(loss='categorical_crossentropy',   # 최적화 함수 지정
+    optimizer='adam',
+    metrics=['accuracy'])
+# 모델 확인
+print(model.summary())
 
-#%%#%% 훈련하기/Trainig fit
+#%%
+
 # 모델 훈련하기
 # 학습 완료된 모델 저장
 hdf5_filename = '\\' + 'weight_defocusing_adam100_Classification_200206.hdf5'
@@ -80,8 +96,8 @@ else:
     # 학습한 모델이 없으면 파일로 저장
     #early_stopping = EarlyStopping(monitor = 'val_loss', patience = 10)
 
-    history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=32, callbacks=[
-    ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=10, verbose=1, mode='auto', min_lr=1e-06)])
+    history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=100, batch_size=32, callbacks=[
+    ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=10, verbose=1, mode='auto', min_lr=1e-04)])
     #history = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=500, batch_size=32, callbacks=[early_stopping])
     model.save_weights(hdf5_file)
     
@@ -91,28 +107,30 @@ else:
     ax[0, 0].set_title('loss')
     ax[0, 0].plot(history.history['loss'], 'r')
     ax[0, 1].set_title('acc')
-    ax[0, 1].plot(history.history['acc'], 'b')
+    ax[0, 1].plot(history.history['accuracy'], 'b')
 
     ax[1, 0].set_title('val_loss')
     ax[1, 0].plot(history.history['val_loss'], 'r--')
     ax[1, 1].set_title('val_acc')
-    ax[1, 1].plot(history.history['val_acc'], 'b--')
+    ax[1, 1].plot(history.history['val_accuracy'], 'b--')
 #%%
 # 모델 평가하기 
 score = model.evaluate(X_test, y_test)
 print('loss=', score[0])        # loss
 print('accuracy=', score[1])    # acc
 
+
 ############################
-#single image 적용
+#특정 이미지에 훈련결과를 적용
 ############################
 # 적용해볼 이미지 
+
 if flag_specific_test_image_appl_0 == True:
     #C:\Users\MDDC\Desktop\2019-02-12 Histopathology dataset\testdata
-    imgdir = "D:\\H&E_dataset\\data\\test_classification"
+    imgdir = "C:\\Users\\MG\\Desktop\\DGMIF\\BTM\\test_sample\\sigma_0.5_sigma_1.9365"
     # if you want file of a specific extension (.png):
     filelist = [f for f in glob.glob(imgdir + "**/*.png", recursive=True)]
-    
+   
     test = []
     for file in filelist:
         img = Image.open(file)
@@ -120,7 +138,6 @@ if flag_specific_test_image_appl_0 == True:
         test.append(img)
     X_train = np.array(test)
     
-    #%% (jgshin) 평가/예측
     # 예측
     #X = X_train.astype("float") / 256
     norm_x = cv.normalize(X_train.astype(np.float64), None, 0, 1, cv.NORM_MINMAX)
@@ -128,10 +145,9 @@ if flag_specific_test_image_appl_0 == True:
     result = [np.argmax(value) for value in pred]   # 예측 값중 가장 높은 클래스 반환
     
     print('Prediction of Sigma Value in Gaussian Blur  : ', categories[result[0]])  
-    
 
 
-#%% (jgshin) 최종 예측
+#%% 최종 훈련결과 표시 / 예측
 #########################################################
 #최종 예측
 #########################################################
@@ -140,12 +156,10 @@ import os, glob
 import numpy as np
 from sklearn.model_selection import train_test_split
 import cv2 as cv
-#%% (jgshin) 최종 예측 prediction
+
 # 분류 대상 카테고리 선택하기 
-#base_dir = "D:\\H&E_dataset\\data\\00x_test_classification"
 base_dir = base_path
-# categories = ["ori","sigma_0.5","sigma_1.0", "sigma_1.5", "sigma_0.5_sigma_1.9365"]
-categories = ["ori","sigma_0.5","sigma_1.0", "sigma_1.5", "sigma_2.0"]
+categories = ["ori", "sigma_0.5", "sigma_1.0", "sigma_1.5", "sigma_2.0"]
 nb_classes = len(categories)
 
 # 이미지 크기 지정 
@@ -184,9 +198,8 @@ else:
     X = np.load(base_path + '\\sigma_classification_trainingdata_X_test_'+ file_timestamp + '.npy')
     Y = np.load(base_path + '\\sigma_classification_trainingdata_Y_test_'+ file_timestamp + '.npy')
     norm_x = cv.normalize(X.astype(np.float64), None, 0, 1, cv.NORM_MINMAX)
-
-#%% (jgshin) 최종 예측 plot array
-# class_names =["ori","sigma_0.5","sigma_1.0", "sigma_1.5", "sigma_0.5_sigma_1.9365"]
+    
+#class_names =["ori","sigma_0.5","sigma_1.0", "sigma_1.5", "sigma_0.5_sigma_1.9365"]
 class_names = ["ori", "sigma_0.5", "sigma_1.0", "sigma_1.5", "sigma_2.0"]
 
 predictions = model.predict(X)
@@ -220,8 +233,8 @@ def plot_value_array(i, predictions_array, true_label):
     thisplot[predicted_label].set_color('red')
     thisplot[np.argmax(true_label)].set_color('blue')
     
-num_rows = 10
-num_cols = 10
+num_rows = 3
+num_cols = 3
 num_images = num_rows * num_cols
 plt.figure(figsize = (2 * 2 * num_cols, 2 * num_rows))
 
